662589c9b8300a58319835adde95448cb9f04b93
Maxwell Koo <mjkoo90@gmail.com>
2018-03-20 17:15:36 -0700

    Testing setup

--- migrations/2_deploy_bounty_registry.js
+++ migrations/2_deploy_bounty_registry.js
@@ -2,6 +2,7 @@ const NectarToken = artifacts.require('NectarToken');
 const BountyRegistry = artifacts.require('BountyRegistry');
 
 module.exports = function(deployer, network, accounts) {
-  const NECTAR_ADDRESS = '0x9e46a38f5daabe8683e10793b06749eef7d733d1';
-  deployer.deploy(BountyRegistry, NECTAR_ADDRESS);
+  deployer.deploy(NectarToken).then(() => {
+    deployer.deploy(BountyRegistry, NectarToken.address);
+  });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,7 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*',
+      gas: 0x2fefd8,
     },
     rinkeby: {
       host: 'localhost',
